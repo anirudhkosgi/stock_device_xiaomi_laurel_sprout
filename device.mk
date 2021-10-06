673fdad6a08f7e02079e0a703edfc51ea6f26e06
TheScarastic <warabhishek@gmail.com>
2021-10-06 07:22:58 +0000

    laurel_sprout: Introduce biometric 2.3 hal
    [Original commit msg]
     * wrap 2.1 hal and add additional function
     * Remove lineage fod hal
     * Rename fodextenssion to udfpsextension
    
    [laurel_sprout specific]
     - move inscreen vintf to separate device manifest file
     - skip udfps ext.
    
    Signed-off-by: anirudhkosgi <kosgi34@gmail.com>

--- device.mk
+++ device.mk
@@ -112,10 +112,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_trinket
-
-PRODUCT_COPY_FILES += \
-    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_trinket
 
 # HIDL
 PRODUCT_PACKAGES += \
