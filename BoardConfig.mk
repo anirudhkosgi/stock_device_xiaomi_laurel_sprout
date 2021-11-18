45d4d2ccb7f5d0133003b25652219d32bb86fa18
Chenyang Zhong <zhongcy95@gmail.com>
2021-11-18 17:06:23 +0530

    laurel_sprout: SEPolicy: Relax sysprop owner enforcement
    Quoting commit afc09932f6fa07c5f6beffbcdc2bb4691e527321 in
    system/sepolicy [1]:
    
    "Devices launching with R or eariler can do this by setting following
    under BoardConfig.mk"
    
    It is impractical to replace every prop definition from qcom to
    vendor_property_type for old platforms like SM6125, so relax the
    enforcement to avoid triggering millions of neverallows.
    
    ref:
    [1] https://android.googlesource.com/platform/system/sepolicy/+/afc09932f6fa07c5f6beffbcdc2bb4691e527321
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: Ic37bae1c9469e5d8f241d55b5a55012fa88656eb
    Signed-off-by: anirudhkosgi <kosgi34@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,6 +93,7 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := vendor/laurel_sprout-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/laurel_sprout
 TARGET_KERNEL_NO_LLVM_BINUTILS := true
+BUILD_BROKEN_ENFORCE_SYSPROP_OWNER := true
 
 
 # Platform
