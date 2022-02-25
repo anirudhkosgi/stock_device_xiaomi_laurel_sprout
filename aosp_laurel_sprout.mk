bdc3a562c75969c4bab793f4ae065d7bac2e0e49
anirudhkosgi <kosgi34@gmail.com>
2022-02-25 22:25:08 +0530

    laurel_sprout: PE bringup

--- aosp_laurel_sprout.mk
+++ aosp_laurel_sprout.mk
@@ -19,15 +19,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-TARGET_BOOT_ANIMATION_RES := 720
-
-TARGET_HAS_FOD := true
+# Inherit some common PixelExperience stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_laurel_sprout
+PRODUCT_NAME := aosp_laurel_sprout
 PRODUCT_DEVICE := laurel_sprout
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A3
@@ -42,3 +38,12 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 ALLOW_MISSING_DEPENDENCIES := true
+
+# Props
+WITH_GAPPS := true
+TARGET_SHIPS_SEPERATE_GAPPS_BUILD := true
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_HAS_FOD := true
+TARGET_SHIP_GCAM_GO := false
